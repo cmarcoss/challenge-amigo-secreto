9c555688598c7617a10a61c7760f520a3133588b
cmarcoss <msqmarck@gmail.com>
2025-08-10 16:14:22 -0300

    crea funcion agrega amigos

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 //declaracion de array para almacenar los nombres
 let amigos=[];
+
+//funcion agregar amigos
+function agregarAmigo()
+{
+    let nombre = document.getElementById("amigo").value.trim();
+    console.log(nombre);
+}
