80b3aa3fa442d888d15d34a38f07f71dec0a4ae5
cmarcoss <msqmarck@gmail.com>
2025-08-10 17:10:44 -0300

    crea funcion mostrar lista amigos

--- app.js
+++ app.js
@@ -17,5 +17,23 @@ function agregarAmigo()
 //limpiar el campo/caja
 function limpiarCaja()
 {
-    document.querySelector('#amigo').value = '';  
+    document.querySelector('#amigo').value = '';
+}
+//mostrar lista de amigos
+function mostrarLista() {
+    // elemento UL donde mostraremos la lista
+    let lista = document.getElementById("listaAmigos");
+
+    // limpiar la lista para evitar duplicados
+    lista.innerHTML = "";
+
+    //Recorrer el array amigos 
+    for (let i = 0; i < amigos.length; i++) {
+        // Crear un nuevo elemento <li> por cada amigo
+        let li = document.createElement("li");
+        li.textContent = amigos[i]; // Asignar el nombre como texto
+
+        //Agregar el <li> dentro del <ul>
+        lista.appendChild(li);
+    }
 }
